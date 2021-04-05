0cf8aac4d5f9842d56f4bbf966f59efd76e36362
aleksei24 <lexa24032@gmail.com>
2021-04-05 12:10:49 +0300

    aux js file has been linked and tested well

---
+++ analytic.js
@@ -0,0 +1,27 @@
+// function to count clicks
+// the file linked in the head oh html (it doesn't manipulate any DOM element)
+
+function createAnalytic() {
+    let counter = 0;
+    let isDestroyed = false;
+
+    const listener = () => counter++;
+
+    document.addEventListener('click', listener);
+
+    return {
+        destroy() {
+            document.removeEventListener('click', listener);
+            isDestroyed = true;
+        },
+
+        getClick() {
+            if (isDestroyed) {
+                return 'Analytic is destroyed';
+            }
+            return counter;
+        },
+    };
+}
+
+window.analytic = createAnalytic();
