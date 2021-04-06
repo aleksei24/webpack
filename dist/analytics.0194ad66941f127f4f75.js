958a763c37f0caf790dc5d9819923facb423260f
aleksei24 <lexa24032@gmail.com>
2021-04-06 14:03:20 +0300

    new ways to assemble have been made

---
+++ dist/analytics.0194ad66941f127f4f75.js
@@ -0,0 +1,1 @@
+window.analytic=function(){let t=0,e=!1;const n=()=>t++;return document.addEventListener("click",n),{destroy(){document.removeEventListener("click",n),e=!0},getClick:()=>e?`Analytic is destroyed. Total amount of clicks = ${t} `:t}}();
