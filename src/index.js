02e8e54c119c400f7c43aca3f764d44d25e36d7b
aleksei24 <lexa24032@gmail.com>
2021-04-09 11:36:39 +0300

    js-library has been linked well

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as $ from 'jquery';
 import Post from '@models/post';
 import json from './assets/myJson';
 import pic from './assets/maya.jpg';
@@ -7,4 +8,6 @@ const post = new Post('Webpack Post Title', pic);
 
 console.log('Post to String', post.toString());
 
-console.log('JSON', json);
+$('pre').html(post.toString());
+
+// console.log('JSON', json);
