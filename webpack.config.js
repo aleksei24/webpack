90995cf751d0bb1c09eca476311f324f434e8c2f
aleksei24 <lexa24032@gmail.com>
2021-04-09 13:59:01 +0300

    favicon has been copied to destination file well

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const FontfacegenWebpackPlugin = require('fontfacegen-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -31,6 +31,14 @@ module.exports = {
             template: './index.html',
         }),
         new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
+            ],
+        }),
     ],
     module: {
         rules: [
