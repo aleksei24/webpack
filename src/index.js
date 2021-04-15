87dbdbc128bde668d7c9de03f47c7715476d5b89
aleksei24 <lexa24032@gmail.com>
2021-04-15 14:53:41 +0300

    dealing with babel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as $ from 'jquery';
 import Post from '@models/post';
 import json from './assets/myJson';
 import pic from './assets/maya.jpg';
+import './babel';
 import './styles/styles.css';
 import './styles/less.less';
 import './styles/scss.scss';
