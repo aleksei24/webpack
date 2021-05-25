e0714a88f82726a30c55ad6cc27ae7cb55ed00fb
aleksei24 <lexa24032@gmail.com>
2021-05-25 10:20:55 +0300

    seems webpack does not reload the page

--- src/babel.js
+++ src/babel.js
@@ -5,6 +5,8 @@ async function start() {
 start().then(console.log);
 
 class Util {
-    static id = Date.now();
+    static id() {
+        Date.now();
+    }
 }
 console.log('Util.id: ', Util.id);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import * as $ from 'jquery';
 import Post from '@models/post';
 import json from './assets/myJson';
 import pic from './assets/maya.jpg';
@@ -14,6 +13,5 @@ console.log('Post to String', post.toString());
 const pre = document.querySelector('pre');
 pre.classList.add('orange');
 pre.innerHTML = post.toString();
-// $('pre').addClass('orange').html(post.toString());
 
 // console.log('JSON', json);
