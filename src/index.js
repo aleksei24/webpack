ac2716b4ec82aec09998af1864313274097304e6
aleksei24 <lexa24032@gmail.com>
2021-06-10 17:49:36 +0300

    an image has been loaded and set into the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import imgMaya from './maya.jpg';
 
 const mainWrapper = document.querySelector('.wrapper');
 
 function testFunction() {
-    const testDiv = document.createElement('div');
+    const firstTitle = document.createElement('div');
 
-    testDiv.innerHTML = _.join(['Hello', 'this is', 'my Webpack'], ' ');
+    firstTitle.innerHTML = _.join(['Hello', 'this is', 'my Webpack'], ' ');
+    firstTitle.classList.add('pink');
 
-    return testDiv;
+    const myFirstImg = new Image();
+    myFirstImg.src = imgMaya;
+
+    firstTitle.appendChild(myFirstImg);
+
+    return firstTitle;
 }
 
 mainWrapper.appendChild(testFunction());
