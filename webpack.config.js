f60efc5279d05c158f620e8eaccdb4b6b26221d7
aleksei24 <lexa24032@gmail.com>
2021-06-16 16:15:42 +0300

    rules for html-webpack-plugin have been set

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: { index: './src/index.js', print: './src/print.js' },
+    plugins: [new htmlWebpackPlugin({ title: 'Output Management' })],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
