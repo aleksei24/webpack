2b1c1214b2be0d9ba6fa2998f02d92f3dd5af746
aleksei24 <lexa24032@gmail.com>
2021-06-16 15:55:36 +0300

    new function has been sent to indexJS and set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 const mainWrapper = document.querySelector('.wrapper');
 
 function testFunction() {
-    const firstTitle = document.createElement('div');
+    const innerDiv = document.createElement('div');
+    const myBtn = document.createElement('button');
 
-    firstTitle.innerHTML = _.join(['Hello', 'this is', 'my Webpack'], ' ');
+    innerDiv.innerHTML = _.join(['Hello', 'this is', 'my Webpack'], ' ');
+    myBtn.innerHTML = 'Poke me and go to the console';
+    myBtn.addEventListener('click', printMe);
+
+    innerDiv.appendChild(myBtn);
 
     return firstTitle;
 }
