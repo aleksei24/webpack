c0e906ac2e76c0433834bae1b8a3dcdb586c02fa
aleksei24 <lexa24032@gmail.com>
2021-06-21 10:37:08 +0300

    start learning development mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: { index: './src/index.js', print: './src/print.js' },
-    plugins: [new htmlWebpackPlugin({ title: 'Output Management' })],
+    plugins: [new htmlWebpackPlugin({ title: 'Development' })],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    mode: 'none',
+    mode: 'development',
 };
