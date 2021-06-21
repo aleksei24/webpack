871590c2fc1a6ece96ef42061d2353df04682c51
aleksei24 <lexa24032@gmail.com>
2021-06-21 10:41:37 +0300

    using source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: { index: './src/index.js', print: './src/print.js' },
+    devtool: 'inline-source-map',
     plugins: [new htmlWebpackPlugin({ title: 'Development' })],
     output: {
         filename: '[name].bundle.js',
