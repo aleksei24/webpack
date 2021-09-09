cb1f2c328de8ebec65bbba3d157dd582c330ca65
aleksei24 <lexa24032@gmail.com>
2021-09-09 16:17:32 +0300

    working on css

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import logVariables from './logVariables';
 
 console.log('ran from index.js');
 logVariables();
+
+// =============================================
+// webpack styles workthrough
+import './style.css';
+
+const headlineContent = 'Welcome';
+const headline = document.querySelector('#main-headline');
+headline.innerText = headlineContent;
