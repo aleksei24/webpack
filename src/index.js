3091080b7930105ebae4ce3d5bba625e378ae71a
aleksei24 <lexa24032@gmail.com>
2021-09-30 01:09:41 +0300

    the image has been appended by means of js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import logVariables from './script/logVariables';
 console.log('ran from index.js');
 logVariables();
 
+// ==========================================
+// img
+import imgOne from './img/nature.jpg';
+const putPic = document.querySelector('#putPic');
+const myImg1 = new Image();
+myImg1.src = imgOne;
+putPic.append(myImg1);
+
 // =============================================
 // webpack styles workthrough
 // import './default.scss';
