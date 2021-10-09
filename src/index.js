8833c114ea8ef19fdf07ffee5d7e8a1297f7263d
aleksei24 <lexa24032@gmail.com>
2021-10-09 12:41:50 +0300

    all linked to index

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import logVariables from './script/logVariables';
 console.log('ran from index.js');
 logVariables();
 
-// ==========================================
-// img
-import imgOne from './img/calmness.webp';
-const putPic = document.querySelector('#putPic');
-const myImg1 = new Image();
-myImg1.src = imgOne;
-putPic.append(myImg1);
-
 // =============================================
 // webpack styles workthrough
 // import './default.scss';
@@ -22,6 +14,7 @@ import 'tailwindcss/utilities.css';
 
 // =============================================
 // script
-import './script/vibes';
-import './script/getData';
-import './script/today';
+// import './script/vibes';
+// import './script/getData';
+// import './script/today';
+import './script/app';
