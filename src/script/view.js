e41bc94a052f6ea9e78fdf7a79a56ccdb8f403be
aleksei24 <lexa24032@gmail.com>
2021-10-10 12:09:12 +0300

    trying to find an altlternative to for-loop

--- src/script/view.js
+++ src/script/view.js
@@ -1,6 +1,12 @@
 const nav = document.querySelector('#nav');
 nav.classList = 'py-4 px-2 bg-green-500 flex items-center justify-around';
-// console.log(nav);
+console.log(nav);
+console.log(nav.children);
+
+for (let i = 0; i < nav.children.length; i++) {
+    const el = nav.children[i];
+    el.classList = 'text-4xl';
+}
 
 const app = document.querySelector('#app');
 app.classList = 'container mx-auto my-6 px-3';
