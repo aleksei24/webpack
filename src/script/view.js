d258ccb010e720c27c0f6022883e5519b0865210
aleksei24 <lexa24032@gmail.com>
2021-10-10 11:01:42 +0300

    styles are invoked progrannatically

--- src/script/view.js
+++ src/script/view.js
@@ -1,3 +1,6 @@
 const nav = document.querySelector('#nav');
 nav.classList = 'py-4 px-2 bg-green-500 flex items-center justify-around';
 // console.log(nav);
+
+const app = document.querySelector('#app');
+app.classList = 'container mx-auto my-6 px-3';
