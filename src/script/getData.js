39bb20eaf5032492418d7acf4377a152fe17bd1e
aleksei24 <lexa24032@gmail.com>
2021-10-11 20:28:21 +0300

    working on list of data

--- src/script/getData.js
+++ src/script/getData.js
@@ -1,25 +1,17 @@
 const currentUrl = `https://jsonplaceholder.typicode.com/users`;
 const displayData = document.querySelector('#displayData');
-const nameData = document.createElement('p');
-const phoneData = document.createElement('p');
-const companyData = document.createElement('p');
-const accomData = document.createElement('p');
-const infoData = [nameData, phoneData, companyData, accomData];
-
-infoData.map((el) => {
-    el.setAttribute('class', 'text-xl');
-});
+const dataList = document.createElement('ul');
 
 fetch(currentUrl)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data),
-            (nameData.textContent = `Name: ${data[4].name}`),
-            (phoneData.textContent = `Phone: ${data[4].phone}`),
-            (accomData.textContent = `Living in: ${data[4].address.city} City on ${data[4].address.street} Street`),
-            (companyData.textContent = `Working for: ${data[4].company.name}`);
-        displayData.append(nameData);
-        displayData.append(phoneData);
-        displayData.append(companyData);
-        displayData.append(accomData);
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            const elemId = data[i].id;
+            const elemUserName = data[i].username;
+            dataList.innerHTML += `<li>${elemId}. ${elemUserName}</li>`;
+            dataList.classList = 'text-xl text-purple-400';
+        }
+
+        displayData.append(dataList);
     });
