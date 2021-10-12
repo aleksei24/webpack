d4709780283bb14f0decf38f9a7da6b5a5355d8f
aleksei24 <lexa24032@gmail.com>
2021-10-12 21:59:58 +0300

    Quiz class is made

---
+++ src/script/models/Quiz.js
@@ -0,0 +1,36 @@
+import { Question } from './Question';
+
+export class Quiz {
+    questionIndex = 0;
+    score = 0;
+    /**
+     *
+     * @param {Question[]} questions
+     */
+    constructor(questions) {
+        this.questions = questions;
+    }
+
+    /**
+     *
+     * @returns {Question} the question found
+     */
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
+
+    isEnded() {
+        return this.questions.length === this.questionIndex;
+    }
+
+    /**
+     *
+     * @param {string} answer some text
+     */
+    guess(answer) {
+        if (this.getQuestionIndex().correctAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++;
+    }
+}
