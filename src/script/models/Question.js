e9fa5ff09c035166a16213169c00a628d7dc6b07
aleksei24 <lexa24032@gmail.com>
2021-10-12 21:58:51 +0300

    params are added

--- src/script/models/Question.js
+++ src/script/models/Question.js
@@ -1,10 +1,21 @@
 export class Question {
+    /**
+     *
+     * @param {string} text the text of the question
+     * @param {string[]} choices the choices of the question
+     * @param {string} answer the answer of the question
+     */
     constructor(text, choices, answer) {
         this.text = text;
         this.choices = choices;
         this.answer = answer;
     }
 
+    /**
+     *
+     * @param {string} choice some text to guess
+     * @returns {boolean} return true if the answer is correct
+     */
     correctAnswer(choice) {
         return choice === this.answer;
     }
