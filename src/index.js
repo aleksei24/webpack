70441d2275cf042e532753635b2b35b11287972d
Aleksei <lexa24032@gmail.com>
2022-06-26 16:55:10 +0300

    dependencies have been upgrded

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import _ from 'lodash';
 import printMe from './print.js';
 function component() {
-    const myDiv = document.createElement('div');
-    myDiv.classList = 'wrapper';
-    myDiv.style.margin = '0 auto';
-    myDiv.style.padding = '10px';
-    const myBtn = document.createElement('button');
-    myDiv.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const myDiv = document.createElement('div');
+  myDiv.classList = 'wrapper';
+  myDiv.style.margin = '0 auto';
+  myDiv.style.padding = '10px';
+  const myBtn = document.createElement('button');
+  myDiv.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    myBtn.innerHTML = 'Poke me and go to the console';
+  myBtn.innerHTML = 'Poke me and go to the console';
 
-    myBtn.addEventListener('click', printMe);
+  myBtn.addEventListener('click', printMe);
 
-    myDiv.appendChild(myBtn);
+  myDiv.appendChild(myBtn);
 
-    return myDiv;
+  return myDiv;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        hot: true,
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            title: 'Webpack Basic Template',
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Webpack Basic Template',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
 };
