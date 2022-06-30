5dea141ea75d08f696f2d80f5e632aa0b22bb17f
Aleksei <lexa24032@gmail.com>
2022-07-01 00:12:18 +0300

    tab size is adjusted

--- webpack.config.js
+++ webpack.config.js
@@ -8,93 +8,93 @@ const { webpack } = require('webpack');
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-    // mode: 'production', // mode by default
-    // mode: 'development',
-    mode: mode,
+  // mode: 'production', // mode by default
+  // mode: 'development',
+  mode: mode,
 
-    entry: './src/index.js',
+  entry: './src/index.js',
 
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Webpack Boilerplate',
-            favicon: './src/img/hard-hat.ico',
-            template: path.resolve(__dirname, './src/template.html'), // template file
-            filename: 'index.html', // output file
-        }),
-        /*new ImageMinimizerPlugin({
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Boilerplate',
+      favicon: './src/img/hard-hat.ico',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    }),
+    /*new ImageMinimizerPlugin({
             minimizerOptions: {
                 plugins: ['jpegtran'],
             },
         }),*/
-        new ImageMinimizerPlugin({
-            deleteOriginalAssets: false,
-            filename: '[path][name].webp',
-            minimizerOptions: { plugins: ['imagemin-webp'] },
-        }),
-        new CleanWebpackPlugin(),
-    ],
+    new ImageMinimizerPlugin({
+      deleteOriginalAssets: false,
+      filename: '[path][name].webp',
+      minimizerOptions: { plugins: ['imagemin-webp'] },
+    }),
+    new CleanWebpackPlugin(),
+  ],
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.(s[ac]|c)ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
                     {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'autoprefixer',
-                                        {
-                                            // options
-                                        },
-                                    ],
-                                ],
-                            },
-                        },
+                      // options
                     },
+                  ],
                 ],
+              },
             },
-            // Images
-            {
-                test: /\.(png|jpg|jpeg|webp)$/i,
-                type: 'asset/resource',
-            },
-            // Fonts and SVGs
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/resource',
-            },
+          },
         ],
-    },
-    // if you need it, just use
-    // devtool: 'source-map', // for production
-    devtool: 'eval-source-map', // for development
+      },
+      // Images
+      {
+        test: /\.(png|jpg|jpeg|webp)$/i,
+        type: 'asset/resource',
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  // if you need it, just use
+  // devtool: 'source-map', // for production
+  devtool: 'eval-source-map', // for development
 
-    devServer: {
-        historyApiFallback: true,
-        open: true,
-        compress: true,
-        hot: false,
-        port: 8080,
-        liveReload: true,
-        watchFiles: ['src/*.html'],
-    },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: false,
+    port: 8080,
+    liveReload: true,
+    watchFiles: ['src/*.html'],
+  },
 };
