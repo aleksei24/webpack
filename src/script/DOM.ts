c39e8ed6498ca809908bfe39d8cb777a117eae7b
Aleksei <lexa24032@gmail.com>
2022-07-12 20:20:40 +0300

    listener for the element is set and works well

--- src/script/DOM.ts
+++ src/script/DOM.ts
@@ -1,1 +1,7 @@
-const headline = document.querySelector('#main-headline');
+const headline = document.querySelector('#main-headline') as HTMLInputElement;
+headline.innerText = 'Hello';
+console.log(headline);
+
+headline.addEventListener('click', (e) => {
+  console.log('event', e);
+})
