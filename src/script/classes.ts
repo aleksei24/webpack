69d180fa8f57ce9836818c3c6bf11e1c4f0a2ddd
Aleksei <lexa24032@gmail.com>
2022-07-14 10:56:02 +0300

    trying to extend class

--- src/script/classes.ts
+++ src/script/classes.ts
@@ -1,18 +1,39 @@
-class Bike {
+interface BikeInterface {
+  showCityToTravel(): string;
+}
+
+class Bike implements BikeInterface {
   model: string;
   year: number;
-
-  // there should be a constructor
-  constructor(model: string, year: number) {
-    this.model = model,
-      this.year = year
+  wheelsDiameter?: number;
+  readonly cityToRide: string = 'Petersburg';
+  // readonly property should have a value
+  static beenFixed = 3;
+  static showNumberItsBeenMended(): number {
+    return Bike.beenFixed;
+    // console.log(Bike.beenFixed);
   }
 
-  production() {
-    return this.year + ' is a year it was made';
+  // there should be a constructor
+  constructor(model: string, year: number, city?: string, wheelsDiameter?: number) {
+    if (city !== undefined) {
+      this.cityToRide = city;
+    }
+    this.model = model,
+      this.year = year,
+      this.wheelsDiameter = wheelsDiameter;
+  }
+
+  showCityToTravel(): string {
+    return `${this.cityToRide} is the city I travelled to with my bike.`;
+  }
+
+  riding(): string {
+    return this.year + ' is a year it has been used since';
   }
 };
 
 const myBike = new Bike('Lorak', 2012);
-console.log(myBike.year);
-console.log(myBike.production());
+console.log(myBike.riding());
+console.log(myBike.showCityToTravel());
+console.log(Bike.showNumberItsBeenMended());
