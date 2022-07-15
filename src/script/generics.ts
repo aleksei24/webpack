2fe31369d1e9cd306ff92801e3761ffb2fe4e56f
Aleksei <lexa24032@gmail.com>
2022-07-15 10:42:04 +0300

    optimizing code with interface

--- src/script/generics.ts
+++ src/script/generics.ts
@@ -5,10 +5,14 @@ function addId<T>(obj: T) {
   }
 }
 
-const jack = {
+interface UserGenericsInterface {
+  name: string,
+}
+
+const jack: UserGenericsInterface = {
   name: 'Jack'
 }
 
-const resultGenerics = addId(jack);
+const resultGenerics = addId<UserGenericsInterface>(jack);
 
 console.log('Generics result: ', resultGenerics);
