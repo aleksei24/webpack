eab25f9c6cc28b7f63328ba4c0d648cbe5da25c2
Aleksei <lexa24032@gmail.com>
2023-03-14 17:49:28 +0300

    trying to print name and age separately

--- src/script/objects.ts
+++ src/script/objects.ts
@@ -1,9 +1,10 @@
 // Objects in typescript are shown through functions
 
-function createCustomer(customer: { name: string, age: number, visitDaily: boolean }) {
-    return `${customer.name} ${customer.age} ${customer.visitDaily}`
+function createCustomer(customer: { name: string, age: number, visitDaily?: boolean }) {
+  return `${customer.name} ${customer.age} ${customer.visitDaily}`
 }
 
 const customer_one = createCustomer({ name: 'Bill', age: 45, visitDaily: false })
 
-console.log(customer_one)
+const myLogOutTwo = document.querySelector('#log_two') as HTMLObjectElement;
+myLogOutTwo.innerText = `${customer_one}`;
