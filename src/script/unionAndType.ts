cfbd201e60fea7cab5b117999ba9a1061389ee76
Aleksei <lexa24032@gmail.com>
2023-03-20 10:35:52 +0300

    working on unions

--- src/script/unionAndType.ts
+++ src/script/unionAndType.ts
@@ -1,19 +1,20 @@
-type ID = string;
-type PopTag = string;
-type KindOfPopTag = PopTag | null;
-
-interface User {
+type ID = number;
+type User = {
   id: ID,
   name: string,
   surname: string;
 }
 
-const popularTags: PopTag[] = ['global heating', 'veganism'];
-const fashionTags: KindOfPopTag = 'what we want';
+type Admin = {
+  id: ID,
+  username: string,
+  alias: string
+}
 
 // the sign | is called union
 
-let username: string = 'Ben';
-let pageNumber: string | number = '1';
-let errorMsg: string | null = null;
-let user: UserInterface | null = null;
+const nick: User | Admin = { username: 'Nick', alias: 'Poop', id: 573 }
+const ben: User | Admin = { name: 'Ben', surname: 'Wolles', id: 390 }
+
+const myLogOutFive = document.querySelector('#log_five') as HTMLObjectElement;
+myLogOutFive.innerText = `Nick's alias - ${nick.alias = 'Wooes'}, Ben's surname - ${ben.surname}.`;
