49c9f6cc528e397e62e42b72d5937666fa01969f
Aleksei <lexa24032@gmail.com>
2023-04-11 11:09:53 +0300

    enum result is printed

--- src/script/enums.ts
+++ src/script/enums.ts
@@ -1,8 +1,11 @@
 enum SeatTheatre {
-  Aisle = 1,
+  Aisle = 5,
   Row,
   Balcony,
   Middle
 }
 
 const mySeat = SeatTheatre.Balcony
+
+const myLogOutEight = document.querySelector('#log_eight') as HTMLObjectElement;
+myLogOutEight.innerText = `Enums: ${mySeat}.`;
