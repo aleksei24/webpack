56e2f200dc9a938b187d2041c0f353e687d70484
Aleksei <lexa24032@gmail.com>
2023-04-24 10:35:00 +0300

    it can be shrunk

--- src/script/generics.ts
+++ src/script/generics.ts
@@ -1,7 +1,7 @@
 const price: Array<number> = [34, 6.89, 10.49]
 const products: Array<string> = ['bread', 'milk', 'millet']
 
-function showArr<Type>(value: Type): Type {
+function showArr<T>(value: T): T {
   return value
 }
 
