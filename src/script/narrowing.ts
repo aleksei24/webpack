1c21f15720c651ee4feb22cbe0aa86a4709d94cd
Aleksei <lexa24032@gmail.com>
2023-04-25 09:55:08 +0300

    learning narrowing

--- src/script/narrowing.ts
+++ src/script/narrowing.ts
@@ -1,3 +1,10 @@
 function checkType(mytype: string | number) {
-  return mytype.toFixed()
+  // should be if statement
+  if (typeof mytype === 'number') {
+    return mytype.toFixed(2)
+  }
+  return mytype.repeat(3)
 }
+
+const myLogOutFifteen = document.querySelector('#log_fifteen') as HTMLObjectElement
+myLogOutFifteen.innerText = `Narrowing: ${checkType(5.4947)} ${checkType('|str|')}`
